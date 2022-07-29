19bb0a6d1b941f655a56aa704f3e95a93b646ca2
trumanpurnell <truman.purnell@gmail.com>
2022-07-29 22:05:47 +0200

    adds suit and rank maps for pprint

--- deck.go
+++ deck.go
@@ -1,5 +1,7 @@
 package pokerface
 
+import "fmt"
+
 type Suit uint8
 type Rank uint8
 
@@ -30,6 +32,34 @@ const (
 type Card struct {
 	Suit Suit
 	Rank Rank
+}
+
+var suitMap = map[Suit]string{
+	Spade:   "♠️",
+	Heart:   "♥️",
+	Club:    "♣️",
+	Diamond: "♦️",
+}
+
+var rankMap = map[Rank]string{
+	One:   "1",
+	Two:   "2",
+	Three: "3",
+	Four:  "4",
+	Five:  "5",
+	Six:   "6",
+	Seven: "7",
+	Eight: "8",
+	Nine:  "9",
+	Ten:   "10",
+	Jack:  "J",
+	Queen: "Q",
+	King:  "K",
+	Ace:   "A",
+}
+
+func (card Card) String() string {
+	return fmt.Sprintf("%v %v", rankMap[card.Rank], suitMap[card.Suit])
 }
 
 type Deck struct {
