680c087adb6b344f3859788833c65e73c8965b3a
trumanpurnell <truman.purnell@gmail.com>
2022-07-31 23:32:21 +0000

    test seems to work but is too slow to verify, time to parallelize

--- test/choose_hands_test.go
+++ test/choose_hands_test.go
@@ -5,65 +5,174 @@ import (
 	"testing"
 
 	"github.com/bb-labs/poker"
+	"golang.org/x/exp/slices"
 )
 
-func TestHandCounts(t *testing.T) {
+// func TestHandCounts(t *testing.T) {
+// 	deck := poker.NewDeck()
+// 	allHands := poker.ChooseN(deck, 5)
+
+// 	tests := []struct {
+// 		PokerHandType poker.PokerHandType
+// 		ExpectedCount int
+// 	}{
+// 		{
+// 			poker.HighCard,
+// 			1_302_540,
+// 		},
+// 		{
+// 			poker.Pair,
+// 			1_098_240,
+// 		},
+// 		{
+// 			poker.TwoPair,
+// 			123_552,
+// 		},
+// 		{
+// 			poker.ThreeOfAKind,
+// 			54_912,
+// 		},
+// 		{
+// 			poker.Straight,
+// 			10_200,
+// 		},
+// 		{
+// 			poker.Flush,
+// 			5_108,
+// 		},
+// 		{
+// 			poker.FullHouse,
+// 			3_744,
+// 		},
+// 		{
+// 			poker.FourOfAKind,
+// 			624,
+// 		},
+// 		{
+// 			poker.StraightFlush,
+// 			40,
+// 		},
+// 	}
+
+// 	for _, test := range tests {
+// 		targetHands := []poker.Hand{}
+
+// 		for _, hand := range allHands {
+// 			pokerHand := poker.NewPokerHand(hand)
+
+// 			if pokerHand.Type == test.PokerHandType {
+// 				targetHands = append(targetHands, pokerHand.Cards)
+// 			}
+// 		}
+
+// 		fmt.Println(test.PokerHandType, len(targetHands), test.ExpectedCount)
+// 	}
+// }
+
+func TestSimpleOrdering(t *testing.T) {
 	deck := poker.NewDeck()
 	allHands := poker.ChooseN(deck, 5)
 
 	tests := []struct {
-		PokerHandType poker.PokerHandType
-		ExpectedCount int
+		PokerHandType        poker.PokerHandType
+		LesserPokerHandTypes []poker.PokerHandType
 	}{
 		{
-			poker.HighCard,
-			1_302_540,
-		},
-		{
 			poker.Pair,
-			1_098_240,
+			[]poker.PokerHandType{
+				poker.HighCard,
+			},
 		},
 		{
 			poker.TwoPair,
-			123_552,
+			[]poker.PokerHandType{
+				poker.HighCard,
+				poker.Pair,
+			},
 		},
 		{
 			poker.ThreeOfAKind,
-			54_912,
+			[]poker.PokerHandType{poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+			},
 		},
 		{
 			poker.Straight,
-			10_200,
+			[]poker.PokerHandType{
+				poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+				poker.ThreeOfAKind,
+			},
 		},
 		{
 			poker.Flush,
-			5_108,
+			[]poker.PokerHandType{poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+				poker.ThreeOfAKind,
+				poker.Straight,
+			},
 		},
 		{
 			poker.FullHouse,
-			3_744,
+			[]poker.PokerHandType{poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+				poker.ThreeOfAKind,
+				poker.Straight,
+				poker.Flush,
+			},
 		},
 		{
 			poker.FourOfAKind,
-			624,
+			[]poker.PokerHandType{poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+				poker.ThreeOfAKind,
+				poker.Straight,
+				poker.Flush,
+				poker.FullHouse,
+			},
 		},
 		{
 			poker.StraightFlush,
-			40,
+			[]poker.PokerHandType{poker.HighCard,
+				poker.Pair,
+				poker.TwoPair,
+				poker.ThreeOfAKind,
+				poker.Straight,
+				poker.Flush,
+				poker.FullHouse,
+				poker.FourOfAKind,
+			},
 		},
 	}
 
-	for _, test := range tests {
-		targetHands := []poker.Hand{}
+	for _, test := range tests[:1] {
+		greaterHands := []poker.PokerHand{}
+		lesserHands := []poker.PokerHand{}
 
 		for _, hand := range allHands {
 			pokerHand := poker.NewPokerHand(hand)
 
 			if pokerHand.Type == test.PokerHandType {
-				targetHands = append(targetHands, pokerHand.Cards)
+				greaterHands = append(greaterHands, pokerHand)
+			} else if slices.Contains(test.LesserPokerHandTypes, pokerHand.Type) {
+				lesserHands = append(lesserHands, pokerHand)
 			}
 		}
 
-		fmt.Println(test.PokerHandType, len(targetHands), test.ExpectedCount)
+		for i, greaterHand := range greaterHands {
+			for _, lesserHand := range lesserHands {
+				if greaterHand.Evaluate() < lesserHand.Evaluate() {
+					fmt.Println("ERROR", greaterHand, lesserHand)
+					return
+				}
+			}
+			fmt.Println(i)
+		}
+
 	}
 }
