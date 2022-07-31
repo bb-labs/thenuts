8f2887c6daadd20ecd65b7dbc13bd85baec595ee
trumanpurnell <truman.purnell@gmail.com>
2022-07-31 23:10:46 +0000

    tests passing but not idempotent

--- test/choose_hands_test.go
+++ test/choose_hands_test.go
@@ -7,29 +7,63 @@ import (
 	"github.com/bb-labs/poker"
 )
 
-func TestChooseHands(t *testing.T) {
+func TestHandCounts(t *testing.T) {
 	deck := poker.NewDeck()
 
-	hands := poker.ChooseN(deck, 5)
-
-	fmt.Println(len(hands))
-}
-
-func TestChooseStraightFlush(t *testing.T) {
-	deck := poker.NewDeck()
-
-	straightFlushHands := []poker.Hand{}
-
-	hands := poker.ChooseN(deck, 5)
-
-	for _, hand := range hands {
-		pokerHand := poker.NewPokerHand(hand)
-
-		if pokerHand.Type == poker.StraightFlush {
-			fmt.Println(pokerHand.Type)
-			straightFlushHands = append(straightFlushHands, pokerHand.Cards)
-		}
+	tests := []struct {
+		PokerHandType poker.PokerHandType
+		ExpectedCount int
+	}{
+		{
+			poker.HighCard,
+			1_302_540,
+		},
+		{
+			poker.Pair,
+			1_098_240,
+		},
+		{
+			poker.TwoPair,
+			123_552,
+		},
+		{
+			poker.ThreeOfAKind,
+			54_912,
+		},
+		{
+			poker.Straight,
+			10_200,
+		},
+		{
+			poker.Flush,
+			5_108,
+		},
+		{
+			poker.FullHouse,
+			3_744,
+		},
+		{
+			poker.FourOfAKind,
+			624,
+		},
+		{
+			poker.StraightFlush,
+			40,
+		},
 	}
 
-	fmt.Println(straightFlushHands, len(straightFlushHands))
+	for _, test := range tests {
+		targetHands := []poker.Hand{}
+		allHands := poker.ChooseN(deck, 5)
+
+		for _, hand := range allHands {
+			pokerHand := poker.NewPokerHand(hand)
+
+			if pokerHand.Type == test.PokerHandType {
+				targetHands = append(targetHands, pokerHand.Cards)
+			}
+		}
+
+		fmt.Println(test.PokerHandType, len(targetHands), test.ExpectedCount)
+	}
 }
